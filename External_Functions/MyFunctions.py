dcd54cdd8f0f624cf68681df3129f6cff3256817
JoeyBowman <ztp370@alumni.ku.dk>
2023-01-17 12:42:25 +0100

    Trying the transformation method function

--- External_Functions/MyFunctions.py
+++ External_Functions/MyFunctions.py
@@ -74,52 +74,127 @@ def matplotlib_plotparameters():
 
 
 def chi2_prob(minuit, dof):
-    '''This function takes in chi2 fit values and the degrees of freedom and returns the probability of the chi2 value.'''
+    '''
+    Description:
+    ------------
+    This function calculates the chi2 probability from a minuit fit.
+    
+    Parameters:
+    -----------
+    minuit : object
+        The minuit object.
+    dof : int
+        The degrees of freedom.
+
+    Returns:
+    --------
+    test : float
+        The chi2 probability.
+    dof : int
+        The degrees of freedom.
+        '''
     chi2    =   minuit.fval
     test = stats.chi2.sf(chi2, dof)
     return test, dof
 
 
- # Calculate ROC curve from two histograms (hist1 is signal, hist2 is background):
-def calc_ROC(hist1, hist2) :
-    '''This functions takes in 2 histograms with the same binning and returns the ROC curve.'''
+# Calculate ROC curves from two datasets (data1 is signal, data2 is background) given a certain bin range
+# and number of bins:
+def roc_curve(sig,bck, bin_range, n_bins, xtra_data=False) :
+    """
+    Description:
+    ------------
+    Calculate ROC curve from two datasets (data1 is signal, data2 is background) given a certain bin range
+    and number of bins.
 
-    # First we extract the entries (y values) and the edges of the histograms:
-    # Note how the "_" is simply used for the rest of what e.g. "hist1" returns (not really of our interest)
-    y_sig, x_sig_edges, _ = hist1 
-    y_bkg, x_bkg_edges, _ = hist2
+    Parameters:
+    -----------
+    sig : array_like
+        The signal data.
+    bck : array_like
+        The background data.
+    bin_range : tuple
+        The range of the bins.
+    n_bins : int
+        The number of bins.
+
+    Returns:
+    --------
+    TPR : array_like
+        The true positive rate.
+    FPR : array_like
+        The false positive rate.
+
+    """
     
-    # Check that the two histograms have the same x edges:
-    if np.array_equal(x_sig_edges, x_bkg_edges) :
-        
-        # Extract the center positions (x values) of the bins (both signal or background works - equal binning)
-        x_centers = 0.5*(x_sig_edges[1:] + x_sig_edges[:-1])
-        
-        # Calculate the integral (sum) of the signal and background:
-        integral_sig = y_sig.sum()
-        integral_bkg = y_bkg.sum()
     
-        # Initialize empty arrays for the True Positive Rate (TPR) and the False Positive Rate (FPR):
-        TPR = np.zeros_like(y_sig) # True positive rate (sensitivity)
-        FPR = np.zeros_like(y_sig) # False positive rate ()
-        
-        # Loop over all bins (x_centers) of the histograms and calculate TN, FP, FN, TP, FPR, and TPR for each bin:
-        for i, x in enumerate(x_centers): 
-            
-            # The cut mask
-            cut = (x_centers < x)
-            
-            # True positive
-            TP = np.sum(y_sig[~cut]) / integral_sig    # True positives
-            FN = np.sum(y_sig[cut]) / integral_sig     # False negatives
-            TPR[i] = TP / (TP + FN)                    # True positive rate
-            
-            # True negative
-            TN = np.sum(y_bkg[cut]) / integral_bkg      # True negatives (background)
-            FP = np.sum(y_bkg[~cut]) / integral_bkg     # False positives
-            FPR[i] = FP / (FP + TN)                     # False positive rate            
-            
+    # Calculate the histogram of the signal and background:
+    hist_sig, bin_edges = np.histogram(sig, bins=n_bins, range=bin_range)
+    hist_bck, bin_edges= np.histogram(bck, bins=n_bins, range=bin_range)
+    
+    # Extract the center positions (x values) of the bins (both signal or background works - equal binning)
+    x_centers = 0.5*(bin_edges[1:] + bin_edges[:-1])
+    
+    # Calculate the integral (sum) of the signal and background:
+    integral_sig = hist_sig.sum()
+    integral_bck = hist_bck.sum()
+    
+    # Initialize empty arrays for the True Positive Rate (TPR) and the False Positive Rate (FPR):
+    TPR = np.cumsum(hist_sig[::-1])[::-1] / integral_sig # True positive rate
+    FPR = np.cumsum(hist_bck[::-1])[::-1] / integral_bck # False positive rate
+    
+    #Calculate the area under the ROC curve:
+    area = np.trapz(TPR, FPR)
+
+
+    if xtra_data == True :
+        return TPR, FPR, x_centers, hist_sig, hist_bck, area
+    else :    
         return FPR, TPR
+
+#Function to calculate Fisher discriminant:
+def fisher_disc(species_a, species_b):
+    """
+    Description:
+    ------------
+    Calculate the Fisher discriminant between two species.
+
+    Parameters:
+    -----------
+    species_a : array_like
+        The data of species A with the parameters in the 2nd index.
     
-    else:
-        AssertionError("Signal and Background histograms have different bins and/or ranges")
+    species_b : array_like
+        The data of species B with the parameters in the 2nd index.
+
+    Returns:
+    --------
+    fisher_a : array_like
+        The Fisher discriminant for species A.
+    fisher_b : array_like
+        The Fisher discriminant for species B.
+    wf : array_like
+        The Fisher weights.
+    """
+
+
+    #Calculate the mean of each species:
+    mean_a = np.mean(species_a, axis=0)
+    mean_b = np.mean(species_b, axis=0)
+
+    #Calculate the covariance matrix each species:
+    cov_a = np.cov(species_a, rowvar=False)
+    cov_b = np.cov(species_b, rowvar=False)
+    cov_sum = cov_a + cov_b
+
+    #The Fisher weights are the inverse of the sum of the covariance matrices:
+    wf = np.dot(np.linalg.inv(cov_sum), (mean_a - mean_b))
+
+
+    #Calculate the Fisher discriminant:
+    fisher_a = np.dot(wf,species_a.T)
+    fisher_b = np.dot(wf,species_b.T)
+
+
+    return fisher_a, fisher_b, wf
+ 
